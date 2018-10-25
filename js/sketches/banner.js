42b888acf09810351f585b625291c2f7e38bca08
Alex Lamson <alex.lamson@gmail.com>
2018-10-24 23:24:38 -0400

    rewrite js so multiple animations can play at once

--- js/sketches/banner.js
+++ js/sketches/banner.js
@@ -1,45 +1,43 @@
-var x = 0.0, y = 0.0;
-var vx = 1.0, vy = 2.0;
+var bannerSketch = function( p ) { // p could be any variable name
+  var x = 0.0, y = 0.0;
+  var vx = 1.0, vy = 2.0;
 
-function setup() {
-  var windowSize = min(windowWidth, windowHeight);
-  var scaling = 0.2;
-  var myCanvas = createCanvas(windowSize*scaling, windowSize*scaling);
-  var myCanvas = createCanvas(windowWidth-15, windowSize*scaling);
-  myCanvas.parent("banner-div");
+  p.setup = function() {
+    var windowSize = p.min(p.windowWidth, p.windowHeight);
+    var scaling = 0.2;
+    p.createCanvas(p.windowWidth-15, windowSize*scaling);
+  };
 
-  // createCanvas(300, 300);
-  // createCanvas(windowWidth, windowHeight);
-  
-  // colorMode(HSL, 255);
-}
+  p.draw = function() {
+    // draw background
+    // p.background(0, 0, 0);
 
-function draw() {
-  background(0, 0, 0, 0);
+    // draw circle
+    p.noStroke();
+    p.fill(85, 168, 224);
+    p.ellipse(x,y,10,10);
 
-  noStroke();
-  fill(85, 168, 224);
-  ellipse(x,y,10,10);
+    // make it move
+    x += vx;
+    y += vy;
 
-  // make it move
-  x += vx;
-  y += vy;
-
-  // make it bounce
-  if(x < 0) {
-    x = 0;
-    vx = -vx;
-  }
-  else if(x > width) {
-    x = width;
-    vx = -vx;
-  }
-  if(y < 0) {
-    y = 0;
-    vy = -vy;
-  }
-  else if(y > height) {
-    y = height;
-    vy = -vy;
-  }
-}
+    // make it bounce
+    if(x < 0) {
+      x = 0;
+      vx = -vx;
+    }
+    else if(x > p.width) {
+      x = p.width;
+      vx = -vx;
+    }
+    if(y < 0) {
+      y = 0;
+      vy = -vy;
+    }
+    else if(y > p.height) {
+      y = p.height;
+      vy = -vy;
+    }
+  };
+};
+var myp5 = new p5(bannerSketch, 'banner-div');
--- js/sketches/wandering_dot.js
+++ js/sketches/wandering_dot.js
@@ -1,98 +1,48 @@
-var xoff1 = 0.0;
-var xoff2 = 1000.0;
-var xoff3 = 2000.0;
-var h = 0;
+// Sketch Two
+var wanderingDotSketch = function( p ) { 
+  var xoff1 = 0.0;
+  var xoff2 = 1000.0;
+  var xoff3 = 2000.0;
+  var h = 0;
 
-function setup() {
-  // var myCanvas = createCanvas(1500, 750);
-  // var myCanvas = createCanvas(400, 400);
-  // var myCanvas = createCanvas(windowWidth*1/2, 400);
-  
-  // var canvasDiv = document.getElementById('wandering-dot-div');
-  // // var canvasDiv = document.getElementById('wandering-dot-div');
-  // console.log(canvasDiv.width);
-  // var windowSize = windowWidth-((15+20+20)*2);//290;//canvasDiv.offsetWidth;
+  p.setup = function() {
+    var windowSize = p.min(p.windowWidth, p.windowHeight);
+    var scaling = 0.5;
+    p.createCanvas(windowSize*scaling, windowSize*scaling);
+    p.colorMode(p.HSL, 255);
+  };
 
-  var windowSize = min(windowWidth, windowHeight);
-  var scaling = 0.5;
-  var myCanvas = createCanvas(windowSize*scaling, windowSize*scaling);
+  p.draw = function() {
+    p.background(0);
+    
 
-  // var scaling = 0.7;
-  // var myCanvas = createCanvas(windowWidth*scaling, windowHeight*scaling);
+    x = p.map(p.noise(xoff1),0,1,0,p.width);
+    y = p.map(p.noise(xoff2),0,1,0,p.height);
+    // xoff1 += 0.01;
+    // xoff2 += 0.01;
 
-  myCanvas.parent("wandering-dot-div");
-  // createCanvas(windowWidth, windowHeight);
-  colorMode(HSL, 255);
-}
+    // xoff1 += p.random(0.03);
+    // xoff2 += p.random(0.03);
 
-function draw() {
-  background(0);
-  
+    // var maxVal = p.map(p.mouseX,0,width,0,0.05);
+    // z = p.map(p.noise(xoff3),0,1,0,maxVal);
 
-  x = map(noise(xoff1),0,1,0,width);
-  y = map(noise(xoff2),0,1,0,height);
-  // xoff1 += 0.01;
-  // xoff2 += 0.01;
+    z = p.map(p.noise(xoff3),0,1,0,0.02);
+    xoff1 += z;
+    xoff2 += z;
+    xoff3 += 0.01;
 
-  // xoff1 += random(0.03);
-  // xoff2 += random(0.03);
+    h += 1;
+    h %= 256;
 
-  // var maxVal = map(mouseX,0,width,0,0.05);
-  // z = map(noise(xoff3),0,1,0,maxVal);
+    // p.textSize(32);
+    // p.text('hue: '+h, 10, 30);
 
-  z = map(noise(xoff3),0,1,0,0.02);
-  xoff1 += z;
-  xoff2 += z;
-  xoff3 += 0.01;
+    // p.fill(255);
+    p.noStroke();
+    p.fill(h, 255, 180);
+    p.ellipse(x,y,10,10);
 
-  h += 1;
-  h %= 256;
-
-  // textSize(32);
-  // text('hue: '+h, 10, 30);
-
-  // fill(255);
-  noStroke();
-  fill(h, 255, 180);
-  ellipse(x,y,10,10);
-}
-
-
-
-// var capture;
-
-// function setup() {
-//   createCanvas(390, 240);
-//   capture = createCapture(VIDEO);
-//   capture.size(320, 240);
-//   //capture.hide();
-// }
-
-// function draw() {
-//   background(255);
-//   image(capture, 0, 0, 320, 240);
-//   filter('INVERT');
-// }
-
-
-
-// function setup(){
-//   createCanvas(710, 400, WEBGL);
-// }
-
-// function draw(){
-//   background(250);
-//   rotateY(frameCount * 0.01);
-
-//   for(var j = 0; j < 5; j++){
-//     push();
-//     for(var i = 0; i < 80; i++){
-//       translate(sin(frameCount * 0.001 + j) * 100, sin(frameCount * 0.001 + j) * 100, i * 0.1);
-//       rotateZ(frameCount * 0.002);
-//       push();
-//       sphere(8, 6, 4);
-//       pop();
-//     }
-//     pop();
-//   }
-// }
+  };
+};
+var myp5 = new p5(wanderingDotSketch, 'wandering-dot-div');
