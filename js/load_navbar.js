6f6e9635e176539e9d52deff967a23bbec3269b9
AlexLamson <alex.lamson@gmail.com>
2019-11-26 19:59:19 -0500

    gotta hit that callback my dude

--- js/load_navbar.js
+++ js/load_navbar.js
@@ -1,9 +1,9 @@
 $(function(){
-    $("#nav-placeholder").load("nav.html");
-
-    // get current URL path and assign 'active' class
-	var pathname = window.location.pathname;
-    var activeLink = $('nav a[href="'+pathname+'"]');
-    activeLink.addClass('active');
-    activeLink.append(' <span class="sr-only">(current)</span>');
+    $("#nav-placeholder").load("nav.html", function() {
+        // get current URL path and assign 'active' class
+        var pathname = window.location.pathname;
+        var activeLink = $('nav a[href="'+pathname+'"]');
+        activeLink.addClass('active');
+        activeLink.append(' <span class="sr-only">(current)</span>');
+    });
 });
